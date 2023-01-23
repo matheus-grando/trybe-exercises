0dcd5d9f3121e55ee4611e79077c28cd3f566a0d
matheus-grando <mthgrando@gmail.com>
2023-01-23 18:50:22 -0300

    Exercicio Foguete

--- 01-fundamentos/java-script/scripts.js
+++ 01-fundamentos/java-script/scripts.js
@@ -39,3 +39,16 @@ else{
     console.log('zero');
 }
 
+let angulo1= 70;
+let angulo2= 80;
+let angulo3= -30;
+
+if (angulo1 + angulo2 + angulo3 === 180) {
+    console.log('true');
+}
+else if (angulo1 || angulo2 || angulo3 < 0){
+    console.log('erro');
+}
+else{
+    console.log('false');
+}
