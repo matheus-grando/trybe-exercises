439e8e85791774128f0aca815c5213efb1ca9044
matheus-grando <mthgrando@gmail.com>
2023-01-23 18:35:32 -0300

    Condicionais resolvidos

---
+++ 01-fundamentos/java-script/scripts.js
@@ -0,0 +1,41 @@
+let a = 13;
+let b = 45;
+let c = 4;
+let num = 13;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+if (a > b) {
+    console.log( 'A é maior que B' );
+}
+else if (a < b) {
+    console.log('B é maior que A');
+}
+else {
+    console.log('Os numeros são iguais.');
+}
+
+if (a > b && a > c) {
+    console.log( a + ' é o maior número');
+}
+else if (b > a && b > c) {
+    console.log( b + ' é o maior número');
+}
+else {
+    console.log( c + ' é o maior número')
+}
+
+if (num % 2 === 0) {
+    console.log('positive');
+}
+else if (num % 2 !== 0) {
+    console.log('negative');
+}
+else{
+    console.log('zero');
+}
+
