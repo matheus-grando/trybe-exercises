426d25016ec5e1e2c570ef1dc4c6987d499b0eb5
matheus-grando <mthgrando@gmail.com>
2023-01-25 20:02:35 -0300

    tentativa terceira questao

--- 01-fundamentos/java-script/dia3/scripts2.js
+++ 01-fundamentos/java-script/dia3/scripts2.js
@@ -17,4 +17,18 @@ for ( let index = word.length - 1; index >= 0; index -=1 ) {
 console.log(reverseWord);
 
 //.
+let bWord = array[0];
+let sWord = array[0];
 
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+for(index = 0; index < array.length; index+=1){
+    if( array[index].length > bWord.length){
+        bWord = array[index];
+    }
+}
+for(index = 0; index < array.length; index+=1){
+    if(array[index].length < sWord.length){
+        sWord = array[index];
+    }
+}
+console.log('Maior palavra é ' + bWord + ', e a menor palavra é '+ sWord);
