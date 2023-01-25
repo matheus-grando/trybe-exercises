0e69206203f8ede28e21ef9a2db8394f3f38cf43
matheus-grando <mthgrando@gmail.com>
2023-01-25 19:31:49 -0300

    Segundo exercicio concluído

--- 01-fundamentos/java-script/dia3/scripts2.js
+++ 01-fundamentos/java-script/dia3/scripts2.js
@@ -10,3 +10,11 @@ console.log(resultFat);
 //.
 
 let word = 'tryber';
+let reverseWord= '';
+for ( let index = word.length - 1; index >= 0; index -=1 ) {
+    reverseWord += word[index];
+}
+console.log(reverseWord);
+
+//.
+
