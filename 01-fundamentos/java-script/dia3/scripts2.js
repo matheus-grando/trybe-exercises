44b6f490bbc05e0ac66f53907297c62151b0d3b5
matheus-grando <mthgrando@gmail.com>
2023-01-25 18:53:36 -0300

    Exercicios do dia 3

---
+++ 01-fundamentos/java-script/dia3/scripts2.js
@@ -0,0 +1,12 @@
+// Exercícios da plataforma on-line//
+
+//.
+let resultFat = 1;
+for(let index = 10; index > 0; index -= 1){
+    resultFat *= index;
+}
+console.log(resultFat);
+
+//.
+
+let word = 'tryber';
